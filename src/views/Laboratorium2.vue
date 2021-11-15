82b139cb93fa323781f63002c9ec78e3d59ac4f8
Kamil Pabin <kpabin9@gmail.com>
2021-11-15 17:57:02 +0100

    Update

--- src/views/Laboratorium2.vue
+++ src/views/Laboratorium2.vue
@@ -2,9 +2,44 @@
   <div class="lab2">
     <div class="opisZadania">
        <h1>Laboratorium 2 - Bootstrap i Javascript</h1>
-       <p>Przykładem wykorzystania Bootstrapa i Javascript, może być zarówno ta strona, jak i strona Escape Roomu, której screenshot załączam poniżej. </p>
-       <div class="newY">
-          <img src="../assets/wyspa1.png">
+       <div id="newY">
+         <div id="tlo">
+            <img id='picc' src="https://upload.wikimedia.org/wikipedia/commons/8/84/Flaga_z_godlem_Rzeczypospolitej_Polskiej.PNG" width="50%">
+         </div>
+       </div>
+       <div class="obiekt">
+         <div>
+           <b-button-group>
+            <b-button class="bbutton" @click="backRed">Czerwone Tło</b-button>
+            <b-button class="bbutton" @click="backPink">Różowe Tło</b-button>
+            <b-button class="bbutton" variant="success" @click="backRandom">Losuj tło</b-button>
+            <b-button class="bbutton" variant="danger" @click="backRes">Reset Tło</b-button>
+          </b-button-group>
+         </div>
+         <div>
+          <b-button-group>
+            <b-button class="bbutton" @click="showToday">Pokaz date</b-button>
+            <b-button class="bbutton" variant="danger" @click="hideToday">Usun date</b-button>
+          </b-button-group>
+         </div>
+         <div>
+           <b-button-group>
+            <b-button class="bbutton" @click="showFlag">Pokaz flage</b-button>
+            <b-button class="bbutton" variant="danger" @click="hideFlag">Usun Flage</b-button>
+          </b-button-group>
+         </div>
+        <div>
+          <b-button-group>
+            <b-button class="bbutton" @click="enableFlag">Wlacz flage</b-button>
+            <b-button class="bbutton" variant="danger" @click="disableFlag">Wylacz Flage</b-button>
+          </b-button-group>
+        </div>
+          <div>
+            <b-button-group>
+              <b-button class="bbutton" @click="rotFlag">Obroc flage</b-button>
+              <b-button class="bbutton" variant="danger" @click="resRotFlag">Resetuj obrot Flagi</b-button>
+            </b-button-group>
+          </div>
        </div>
        <p>Na obu stronach znajduje się dużo funkcjonalnego Javascriptu, jak i elementy z Bootstrapa. Samych zmian dostępnych przez javascript jest spokojnie ponad 20, m.in. zmiana tła, chowanie żarówek, chowanie elementów tekstu, uruchamianie skryptów php, zmiana prędkości sekundy, pokazywanie podpowiedzi, zmiana widocznego elementu, postępujący pasek itd.  </p>
     </div>
@@ -15,6 +50,66 @@
 export default {
   name: 'Laboratorium2',
   components: {
+  },
+  methods: {
+    // zabawa kolorem tla diva
+    backRed: function () {
+      document.getElementById('newY').style.background = 'red'
+    },
+    backPink: function () {
+      document.getElementById('newY').style.background = 'pink'
+    },
+    backRandom: function () {
+      var x = Math.floor(Math.random() * 256)
+      var y = Math.floor(Math.random() * 256)
+      var z = Math.floor(Math.random() * 256)
+      var bgColor = 'rgb(' + x + ',' + y + ',' + z + ')'
+      document.getElementById('newY').style.background = bgColor
+    },
+    backRes: function () {
+      document.getElementById('newY').style.background = 'transparent'
+    },
+    // dodawanie calkiem nowego elementu
+    showToday: function () {
+      var today = new Date()
+      const para = document.createElement('p')
+      para.setAttribute('id', 'para-1')
+      const node = document.createTextNode(today)
+      para.appendChild(node)
+      const element = document.getElementById('tlo')
+      element.appendChild(para)
+    },
+    // usuwanie tego elementu po id
+    hideToday: function () {
+      const elmnt = document.getElementById('para-1')
+      elmnt.remove()
+    },
+    showFlag: function () {
+      document.getElementById('picc').style.visibility = 'visible'
+    },
+    hideFlag: function () {
+      document.getElementById('picc').style.visibility = 'hidden'
+    },
+    enableFlag: function () {
+      document.getElementById('picc').style.display = 'block'
+    },
+    disableFlag: function () {
+      document.getElementById('picc').style.display = 'none'
+    },
+    rotFlag: function () {
+      document.getElementById('picc').style.transform = 'rotate(180deg)'
+    },
+    resRotFlag: function () {
+      document.getElementById('picc').style.transform = 'rotate(0deg)'
+    }
+
+  },
+  data () {
+    return {
+      tloRed: false,
+      values: {
+      }
+    }
   }
 }
 </script>
@@ -30,16 +125,37 @@ export default {
   margin:0;
   max-height:100%;
 }
+.obiekt{
+  padding:1px;
+  display:flexbox;
+}
+.obiekt .bbutton{
+  margin:2px;
+}
 .opisZadania{
   font-family: open-sans;
   font-size:1.3rem;
 }
-.newY{
+#newY{
   display:block;
   margin:auto;
-  max-height:60%;
+  height:50vh;
+  width:100%;
+  padding:auto;
+  background:blue;
 }
-.newY img{
-  max-width: 60%;
+#tlo{
+  margin:auto;
+  height:100%;
+  width:50%;
+  background:transparent;
+  display:flexbox;
+}
+#tlo img{
+  margin:auto;
+}
+#flaga{
+  height:50%;
+  width:60%;
 }
 </style>
--- src/views/Laboratorium3.vue
+++ src/views/Laboratorium3.vue
@@ -8,6 +8,10 @@
        </div>
        <p>Generalnie całe vue.js na tym bazuje, ale jakby wskazać coś konkretnego poza samym systemem tej strony, to można wskazać własne zdarzenia, takie jak np. 'nasłuchiwanie' na wartość zmiennej, a gdy ona się zmieni to pojawia się element. Ciekawym przykładem może być funkcjonalność formularza połączona z pisaniem listu w laboratorium 1, lub żarówka na stronie głównej, która czeka na kliknięcie w nią a następnie ładuje drugą. </p>
        <p>Kolejnym przykładem może być większość strony escape roomu, która bazuje na oczekiwaniu aż administrator (pokoju) kliknie na przyciski, lub gdy jakaś wartość ulegnie zmianie.</p>
+       <b-button-group>
+        <b-button href="https://github.com/kamil-pabin/WebDev" target="_blank">Repo</b-button>
+        <b-button href="https://kamil-pabin.github.io/WebDev/frontend/escaperoom/" target="_blank">Demo</b-button>
+       </b-button-group>
        <div class="newY">
          <img src="../assets/fun1.png">
        </div>
