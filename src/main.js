8bdae1c7acb6486cb32acb8efea9fa064991af88
Kamil Pabin <kpabin9@gmail.com>
2022-04-22 16:08:26 +0200

    Update of config files

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import VueFormulate from '@braid/vue-formulate'
 import { AlertPlugin, BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import LoadScript from 'vue-plugin-load-script'
 
 Vue.config.productionTip = false
 
+Vue.use(LoadScript)
 Vue.use(AlertPlugin)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
--- src/views/Laboratorium4.vue
+++ src/views/Laboratorium4.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="lab4">
     <div class="opisZadania">
-       <h1>Laboratorium 4 - Gra</h1>
-       <p>W przyszłości planowana gra platformowa oparta na widokach vue.js </p>
+      <h1>Laboratorium 4 - Gra</h1>
+      <p>W przyszłości planowana gra platformowa oparta na widokach vue.js</p>
+    </div>
+    <div class="container"></div>
+    <div id="controls">
+      <button type="button" class="btn primary" data-action="newGame">
+        Zacznij od nowa
+      </button>
+      <button type="button" class="btn primary" data-action="solve">
+        Pokaż rozwiązanie
+      </button>
+      <button type="button" class="btn primary" data-action="validate">
+        Sprawdź
+      </button>
     </div>
   </div>
 </template>
@@ -10,32 +22,36 @@
 <script>
 export default {
   name: 'Laboratorium4',
-  components: {
-  }
+  components: {},
+  methods: {}
+
 }
 </script>
-
 <style lang="scss" scoped>
-.lab4{
+.lab4 {
   font-family: Zen Kurenaido;
   text-align: center;
-  width:100%;
-  position:flex;
+  width: 100%;
+  position: flex;
   color: black;
-  padding:0;
-  margin:0;
-  max-height:100%;
+  padding: 0;
+  margin: 0;
+  max-height: 100%;
 }
-.opisZadania{
+.opisZadania {
   font-family: open-sans;
-  font-size:1.3rem;
+  font-size: 1.3rem;
 }
-.newY{
-  display:block;
-  margin:auto;
-  max-height:60%;
+.newY {
+  display: block;
+  margin: auto;
+  max-height: 60%;
 }
-.newY img{
+.newY img {
   max-width: 60%;
 }
 </style>
+
+<script>
+
+</script>
